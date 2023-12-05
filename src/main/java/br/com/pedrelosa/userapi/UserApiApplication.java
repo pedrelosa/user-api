2aa1fe1d3f643709277f3b2c42023c005024dbe6
Pedrelosa <pedrelosa@hotmail.com>
2023-12-04 21:34:54 -0300

    feat- implementing a CommandLineRunner on initialization for persist a User every time db start

--- src/main/java/br/com/pedrelosa/userapi/UserApiApplication.java
+++ src/main/java/br/com/pedrelosa/userapi/UserApiApplication.java
@@ -1,13 +1,28 @@
 package br.com.pedrelosa.userapi;
 
+import br.com.pedrelosa.userapi.domain.User;
+import br.com.pedrelosa.userapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class UserApiApplication {
+public class UserApiApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.saveAll(List.of(
+                new User(null, "Pedro Elo", "pedrelosa@hotmail.com", "123", 20.0)
+        ));
+    }
 }
